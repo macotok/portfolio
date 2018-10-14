96ebdb0ce201b448c4e6e6a97a2a0e92671dbd0b
macotok <monochromer@r8.ucom.ne.jp>
2018-10-14 23:24:31 +0900

    fix about text in top

--- src/components/About.vue
+++ src/components/About.vue
@@ -8,7 +8,7 @@
   <div v-if="page === 'top'" class="aboutBlock">
     <p>
       簡単な経歴を紹介します。<br>
-      ちなみにこのサイトはViewは「Vue.js」。Serverは「Firebase」で作りました。
+      ちなみにこのサイトはViewは「Vue.js」。Serverは「Firebase」で作りました。(※レスポンシブ対応してません。)
     </p>
   </div>
 </template>

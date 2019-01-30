4c626eed88d086132367758f40332fd87ff16b25
macotok <monochromer@r8.ucom.ne.jp>
2019-01-31 00:27:35 +0900

    fix .env

--- config/dev.env.js
+++ config/dev.env.js
@@ -1,7 +1,7 @@
 'use strict';
-const merge = require('webpack-merge')
-const prodEnv = require('./prod.env')
-const FIRE_BASE = require('../.env.js');
+const merge = require('webpack-merge');
+const prodEnv = require('./prod.env');
+const FIRE_BASE = require('../.env');
 
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',

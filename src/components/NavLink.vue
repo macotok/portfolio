4ea6596d9ff3ca72d23682eae0110803389405c7
macotok <monochromer@r8.ucom.ne.jp>
2019-02-26 00:54:05 +0900

    style responsive

--- src/components/NavLink.vue
+++ src/components/NavLink.vue
@@ -28,17 +28,19 @@
 </style>
 
 <template>
-  <nav>
-    <ul>
-      <li><router-link :to="{name: 'root'}" exact>Top</router-link></li>
-      <li><router-link :to="{name: 'works'}">Works</router-link></li>
-      <li><router-link :to="{name: 'skills'}">Skills</router-link></li>
-      <li><router-link :to="{name: 'profile'}">Profile</router-link></li>
-      <li v-if="isAdmin"><router-link :to="{name: 'addWork'}">add Work</router-link></li>
-      <li v-if="isAdmin"><router-link :to="{name: 'addSkill'}">add Skill</router-link></li>
-      <li v-if="isAdmin"><router-link :to="{name: 'admin'}">Admin</router-link></li>
-    </ul>
-  </nav>
+  <div class="m-nav-01">
+    <nav>
+      <ul>
+        <li><router-link :to="{name: 'root'}" exact>Top</router-link></li>
+        <li><router-link :to="{name: 'works'}">Works</router-link></li>
+        <li><router-link :to="{name: 'skills'}">Skills</router-link></li>
+        <li><router-link :to="{name: 'profile'}">Profile</router-link></li>
+        <li v-if="isAdmin"><router-link :to="{name: 'addWork'}">add Work</router-link></li>
+        <li v-if="isAdmin"><router-link :to="{name: 'addSkill'}">add Skill</router-link></li>
+        <li v-if="isAdmin"><router-link :to="{name: 'admin'}">Admin</router-link></li>
+      </ul>
+    </nav>
+  </div>
 </template>
 
 <script>

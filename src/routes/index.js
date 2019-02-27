94cdda438fc28e05279c0e068748353ddb2718d6
macotok <monochromer@r8.ucom.ne.jp>
2019-02-28 07:28:45 +0900

    add savedPosition to routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -63,7 +63,10 @@ export default {
       name: 'admin',
     },
   ],
-  scrollBehavior() {
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition;
+    }
     return { x: 0, y: 0 };
   },
 };

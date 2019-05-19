0cbd0b8b5c2ac1d24e4a08e92188b0ae19261399
macotok <monochromer@r8.ucom.ne.jp>
2019-05-19 12:42:31 +0900

    add atLinkImage for profile

---
+++ src/components/organisms/Profile/OrSideBlock.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+  div
+    at-image(
+      src="/images/profile.jpg"
+      alt="@macotok"
+      width="200"
+    )
+    at-text @macotok
+    at-link-image(
+      href="https://github.com/macotok"
+      target="_blank"
+      src="/images/github.svg"
+      alt="github"
+      width="20"
+    )
+    at-link-image(
+      href="https://qiita.com/macotok"
+      target="_blank"
+      src="/images/qiita.svg"
+      alt="qiita"
+      width="20"
+    )
+</template>
+
+<script>
+import AtImage from '@/components/atoms/image/AtImage';
+import AtLinkImage from '@/components/atoms/image/AtLinkImage';
+import AtText from '@/components/atoms/text/AtText';
+
+export default {
+  components: {
+    AtImage,
+    AtLinkImage,
+    AtText,
+  },
+};
+</script>
--- src/components/templates/TeProfile.vue
+++ src/components/templates/TeProfile.vue
@@ -4,6 +4,7 @@
     or-header-nav
     .l-container
       at-title-h2 Profile
+      or-side-block
     or-footer @portfolio-205d8.firebaseapp.com
 </template>
 
@@ -12,6 +13,7 @@ import AtTitleH2 from '@/components/atoms/text/AtTitleH2';
 import OrFooter from '@/components/organisms/OrFooter';
 import OrHeader from '@/components/organisms/OrHeader';
 import OrHeaderNav from '@/components/organisms/OrHeaderNav';
+import OrSideBlock from '@/components/organisms/Profile/OrSideBlock';
 
 export default {
   components: {
@@ -19,6 +21,7 @@ export default {
     OrFooter,
     OrHeader,
     OrHeaderNav,
+    OrSideBlock,
   },
 };
 </script>

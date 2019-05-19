f2fe78e4c3924ec65750e662587eb50957356072
macotok <monochromer@r8.ucom.ne.jp>
2019-05-19 11:45:24 +0900

    add image to atom

---
+++ src/components/atoms/image/AtImage.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+  img(
+    :src="src"
+    :alt="alt"
+    :width="width"
+    :height="height"
+  )
+</template>
+
+<script>
+export default {
+  props: {
+    src: {
+      type: String,
+      required: true,
+    },
+    alt: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: [String, Number],
+      default: 'auto',
+    },
+    height: {
+      type: [String, Number],
+      default: 'auto',
+    },
+  },
+};
+</script>

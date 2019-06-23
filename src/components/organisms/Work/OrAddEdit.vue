6500a2ce8df1e8381a9d302aa620c04dd39ace38
macotok <monochromer@r8.ucom.ne.jp>
2019-06-23 17:36:09 +0900

    workの全項目のエラーメッセージをdictionaryで管理

--- src/components/organisms/Work/OrAddEdit.vue
+++ src/components/organisms/Work/OrAddEdit.vue
@@ -89,7 +89,6 @@ export default {
         },
         url: {
           required: true,
-          min: 20,
           url: {
             require_protocol: true,
           },
--- src/validate/disctionary.js
+++ src/validate/disctionary.js
@@ -1,7 +1,17 @@
 const dictionary = {
   custom: {
     title: {
-      required: 'タイトルは必須です',
+      required: 'タイトルを入力してください。',
+    },
+    tags: {
+      required: 'タグを入力してください。複数指定するときはカンマ区切りで入力してください。',
+    },
+    url: {
+      required: 'URLを入力してください。',
+      url: 'URLの形式が違っています。',
+    },
+    text: {
+      required: '内容を入力してください。',
     },
   },
 };

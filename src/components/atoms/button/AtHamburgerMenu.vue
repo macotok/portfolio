f0ec2c123a1ffa30ea6e089d4f3ec305ebfd1616
macotok <monochromer@r8.ucom.ne.jp>
2019-07-28 10:46:51 +0900

    ハンバーガーメニューを表示

---
+++ src/components/atoms/button/AtHamburgerMenu.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+  div.atHamburgerMenu(
+    @click="clickButton"
+  )
+    div.atHamburgerMenu-item
+    div.atHamburgerMenu-item
+</template>
+
+<script>
+export default {
+  methods: {
+    clickButton() {
+      this.$emit('click-button');
+    },
+  },
+};
+</script>
--- src/components/organisms/OrHeader.vue
+++ src/components/organisms/OrHeader.vue
@@ -1,14 +1,18 @@
 <template lang="pug">
   header.orHeader
     at-title-h1 Portfolio
+    div.orHeader-icon
+      at-hamburger-menu
 </template>
 
 <script>
 import AtTitleH1 from '@/components/atoms/text/AtTitleH1';
+import AtHamburgerMenu from '@/components/atoms/button/AtHamburgerMenu';
 
 export default {
   components: {
     AtTitleH1,
+    AtHamburgerMenu,
   },
 };
 </script>

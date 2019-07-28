0ae36ba58c0356b0f972aff304c14b2f6318e690
macotok <monochromer@r8.ucom.ne.jp>
2019-07-28 13:46:27 +0900

    ドロワーメニューのモーダル実装

---
+++ src/components/molecules/modal/MoDrawerMenu.vue
@@ -0,0 +1,34 @@
+<template lang="pug">
+  div.moDrawerMenu
+    at-modal(
+      :modalName="modalName"
+      :width="width"
+      :height="height"
+    )
+      div.moDrawerMenu-inner
+        p もだ
+</template>
+
+<script>
+import AtModal from '@/components/atoms/modal/AtModal';
+
+export default {
+  components: {
+    AtModal,
+  },
+  props: {
+    modalName: {
+      type: String,
+      required: true,
+    },
+    width: {
+      type: String,
+      required: true,
+    },
+    height: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
--- src/components/organisms/OrHeader.vue
+++ src/components/organisms/OrHeader.vue
@@ -2,17 +2,35 @@
   header.orHeader
     at-title-h1 Portfolio
     div.orHeader-icon
-      at-hamburger-menu
+      at-hamburger-menu(
+        @click-button="clickButton"
+      )
+    MoDrawerMenu(
+      :modalName="DRAWER_MENU"
+      width="100%"
+      height="100%"
+    )
 </template>
 
 <script>
+import { DRAWER_MENU } from '@/defines';
 import AtTitleH1 from '@/components/atoms/text/AtTitleH1';
 import AtHamburgerMenu from '@/components/atoms/button/AtHamburgerMenu';
+import MoDrawerMenu from '@/components/molecules/modal/MoDrawerMenu';
 
 export default {
   components: {
     AtTitleH1,
     AtHamburgerMenu,
+    MoDrawerMenu,
+  },
+  computed: {
+    DRAWER_MENU: () => DRAWER_MENU,
+  },
+  methods: {
+    clickButton() {
+      this.$modal.show(DRAWER_MENU);
+    },
   },
 };
 </script>

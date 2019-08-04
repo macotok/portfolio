c7306b3b5aa8808475438df14f742549e718dbc3
macotok <monochromer@r8.ucom.ne.jp>
2019-08-04 09:49:25 +0900

    work/skillの画面に投稿がないときのメッセージを設定

--- src/components/templates/Skill/TeIndex.vue
+++ src/components/templates/Skill/TeIndex.vue
@@ -4,15 +4,23 @@
     or-header-nav
     div.l-container
       at-title-h2 Skill
-      or-list(
-        :dataList="skillList"
-        :isLoginAdmin="isLoginAdmin"
+      template(
+        v-if="skillList.length"
       )
+        or-list(
+          :dataList="skillList"
+          :isLoginAdmin="isLoginAdmin"
+        )
+      at-text(
+        v-else
+      ) {{ NOT_POST_MESSAGE }}
     or-footer
 </template>
 
 <script>
+import { NOT_POST_MESSAGE } from '@/defines';
 import AtTitleH2 from '@/components/atoms/text/AtTitleH2';
+import AtText from '@/components/atoms/text/AtText';
 import OrFooter from '@/components/organisms/OrFooter';
 import OrHeader from '@/components/organisms/OrHeader';
 import OrHeaderNav from '@/components/organisms/OrHeaderNav';
@@ -20,6 +28,7 @@ import OrList from '@/components/organisms/Skill/OrList';
 
 export default {
   components: {
+    AtText,
     AtTitleH2,
     OrFooter,
     OrHeader,
@@ -35,6 +44,9 @@ export default {
       type: Boolean,
       required: true,
     },
+  },
+  computed: {
+    NOT_POST_MESSAGE: () => NOT_POST_MESSAGE,
   },
 };
 </script>
--- src/components/templates/Work/TeIndex.vue
+++ src/components/templates/Work/TeIndex.vue
@@ -4,19 +4,27 @@
     or-header-nav
     div.l-container
       at-title-h2 Work
-      or-list-pager(
-        pageType="work"
-        targetKey="db"
-        :actionType="AC_PAGER_NUMBER"
-        :list="workList"
-        :pagerNumber="pagerNumber"
-        :length="workLength"
+      template(
+        v-if="workList.length"
       )
+        or-list-pager(
+          pageType="work"
+          targetKey="db"
+          :actionType="AC_PAGER_NUMBER"
+          :list="workList"
+          :pagerNumber="pagerNumber"
+          :length="workLength"
+        )
+      at-text(
+        v-else
+      ) {{ NOT_POST_MESSAGE }}
     or-footer
 </template>
 
 <script>
+import { NOT_POST_MESSAGE } from '@/defines';
 import { AC_PAGER_NUMBER } from '@/store/work/actions/pagerNumber';
+import AtText from '@/components/atoms/text/AtText';
 import AtTitleH2 from '@/components/atoms/text/AtTitleH2';
 import OrFooter from '@/components/organisms/OrFooter';
 import OrHeader from '@/components/organisms/OrHeader';
@@ -25,6 +33,7 @@ import OrListPager from '@/components/organisms/OrListPager';
 
 export default {
   components: {
+    AtText,
     AtTitleH2,
     OrFooter,
     OrHeader,
@@ -47,6 +56,7 @@ export default {
   },
   computed: {
     AC_PAGER_NUMBER: () => (AC_PAGER_NUMBER),
+    NOT_POST_MESSAGE: () => NOT_POST_MESSAGE,
   },
 };
 </script>

8649c1005e6c0f59f292eb13c51ceae8d87f0fb0
macotok <monochromer@r8.ucom.ne.jp>
2019-08-12 13:48:49 +0900

    文言修正。develop→development

--- src/store/admin/state/index.js
+++ src/store/admin/state/index.js
@@ -1,4 +1,4 @@
 const state = {
-  isLoginAdmin: (process.env.SWITCH_DATABASE === 'develop') || false,
+  isLoginAdmin: (process.env.SWITCH_DATABASE === 'development') || false,
 };
 export default state;

8b9413aa4b59a7c58e063f22e6e0f73942c30cfa
macotok <monochromer@r8.ucom.ne.jp>
2019-09-15 12:44:37 +0900

    更新日時順をsortするjsファイルをtypescriptで記述

--- src/utils/sortUpdatedAt.js
+++ src/utils/sortUpdatedAt.js
@@ -1,5 +1,4 @@
 export default function sortUpdatedAt(dataList) {
-    console.log(dataList);
     dataList.sort(function (a, b) { return (a.updatedAt < b.updatedAt ? 1 : -1); });
     return dataList;
 }
--- src/utils/sortUpdatedAt.ts
+++ src/utils/sortUpdatedAt.ts
@@ -1,5 +1,20 @@
-export default function sortUpdatedAt(dataList) {
-  console.log(dataList)
+interface data {
+  createdAt: string,
+  id: number,
+  image: {
+    name: string,
+    path: string,
+  },
+  image_name: string,
+  image_path: string,
+  tags: string[],
+  text: string,
+  title: string,
+  updatedAt: string,
+  url: string,
+}
+
+export default function sortUpdatedAt(dataList: data[]): data[] {
   dataList.sort((a, b) => (
     a.updatedAt < b.updatedAt ? 1 : -1
   ));

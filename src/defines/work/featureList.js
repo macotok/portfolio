61ba91ad8b05fed2a471ffc94d13d4151b6d3836
macotok <monochromer@r8.ucom.ne.jp>
2019-09-22 17:53:24 +0900

    機能一覧を更新

--- src/defines/work/featureList.js
+++ src/defines/work/featureList.js
@@ -1,15 +1,55 @@
 const featureList = [
   {
+    id: 24,
+    feature:
+      [
+        {
+          name: 'React/Redux/Material-UIでView側を制作',
+          role: '開発作業',
+        },
+        {
+          name: 'サーバーサイドは「Firebase」で制作',
+          role: '開発作業',
+        },
+      ],
+  },
+  {
     id: 23,
     feature:
       [
         {
-          name: '「イオンのお得なお買い物情報」「おすすめキャンヘーン」「周囲の店舗のお得な情報」「今日は何の日?」',
+          name: '「イオンのお得なお買い物情報」「おすすめキャンペーン」「周囲の店舗のお得な情報」「今日は何の日?」',
           role: '開発要件定義/開発設計/開発工数出し/進捗管理',
         },
         {
-          name: 'ヒシュアルの「ローテバナー」',
-          role: '開発要件定義/開発設計/開発工数出し/進捗管理',
+          name: 'ビジュアルの「ローテバナー」',
+          role: 'Reactで制作したCM􏰂Sで機能実装(画像登録/表示日時/保存データをA􏰃PIで投げ処理)',
+        },
+        {
+          name: '「お気に入り店舗」',
+          role: '都道府県店舗のAP􏰃Iデータをパラメーターで市区町村ごとに振り分ける開発設計',
+        },
+        {
+          name: '保存できない不具合を処理',
+          role: '􏰁Reactで制作したCMSで保存してAP􏰃Iにrequestする処理かできない不具合を解決',
+        },
+      ],
+  },
+  {
+    id: 22,
+    feature:
+      [
+        {
+          name: '「https://paso-tokyo.com/」「https://jewelry.paso-tokyo.com/」の全画面',
+          role: 'Pug/Sass/Javascript/WordPressで開発',
+        },
+        {
+          name: '「News」「Collection」「Exhibition」の投稿機能',
+          role: 'WordPressのカスタム投稿タイプで開発',
+        },
+        {
+          name: '「Contact」のお問い合わせ機能',
+          role: 'WordPressのプラグインで開発',
         },
       ],
   },
